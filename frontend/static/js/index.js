8b9b2a924c78f59795116addbf58c79a4e70fe68
Bartosz Woliński <55801868+Hikkaruu@users.noreply.github.com>
2025-02-01 22:02:15 +0100

    Added volts and amps display on mulimeters

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -45,7 +45,20 @@ const multimeterMode = {
   10: { degree: 336, mode: "DCA 2m" },
   11: { degree: 348, mode: "DCA 200u" },
 };
-let rotationsKnob1 = 1;
+
+const knob1Voltage = {
+  0: { varistor: 0, photoresistor: 0 },
+  1: { varistor: 37.5, photoresistor: 1.5 },
+  2: { varistor: 37.5*2, photoresistor: 3 },
+  3: { varistor: 37.5*3, photoresistor: 5 },
+  4: { varistor: 37.5*4, photoresistor: 8 },
+  5: { varistor: 37.5*5, photoresistor: 11 },
+  6: { varistor: 37.5*6, photoresistor: 16 },
+  7: { varistor: 37.5*7, photoresistor: 20 },
+  8: { varistor: 300, photoresistor: 24 },
+};
+
+let rotationsKnob1 = 0;
 let rotationsKnob2 = 1;
 let rotationsMultimeterKnob1 = 1;
 let currentMode = mode[0];
@@ -53,11 +66,80 @@ let currentMultimeterMode = multimeterMode[0].mode;
 let rotationsMultimeterKnob2 = 1;
 let currentMultimeterMode2 = multimeterMode[0].mode;
 let chartInstance;
+let multimeterDisplay1 = document.getElementById("multimeter1-value");
+let multimeterDisplay2 = document.getElementById("multimeter2-value");
+let multimeter1Active = true;
+let multimeter2Active = true;
+let resistance = 1000;
+
+
+function getDisplayValue(currentMultimeterMode) {
+  if (currentMode === "OFF") return "OFF";
+  switch (currentMultimeterMode) {
+    case "OFF":
+      return "OFF";
+    case "DCV 200m":
+      return calculateDisplayValue(getVoltage(), 0.2, 3);
+    case "DCV 2":
+      return calculateDisplayValue(getVoltage(), 2, 3);
+    case "DCV 20":
+      return calculateDisplayValue(getVoltage(), 20, 2);
+    case "DCV 200":
+      return calculateDisplayValue(getVoltage(), 200, 1);
+    case "DCV 1000":
+      return calculateDisplayValue(getVoltage(), 1000, 0);
+    case "DCA 20":
+      return calculateDisplayValue(getVoltage()/resistance, 20, 1)
+    case "DCA 2":
+      return calculateDisplayValue(getVoltage()/resistance, 2, 2)
+    case "DCA 200m":
+      return calculateDisplayValue(getVoltage()/resistance, 0.2, 3)
+    case "DCA 20m":
+      return calculateDisplayValue(getVoltage()/resistance, 0.02, 3)
+    case "DCA 2m":
+      return calculateDisplayValue(getVoltage()/resistance, 0.002, 3)
+    case "DCA 200u":
+      return calculateDisplayValue(getVoltage()/resistance, 0.0002, 3)
+    default:
+      return 'OFF'
+  }
+}
+
+function calculateDisplayValue(value, range, fix){
+  if (value > range) return "0L";
+  return valueToFixed(value, fix);
+}
+
+function valueToFixed(value, fix) {
+  if (typeof value === 'number') return value.toFixed(fix);
+}
+
+function getVoltage() {
+  let voltage = "OFF";
+  if (currentMode === "PHOTORESISTOR") {
+    voltage = knob1Voltage[rotationsKnob1].photoresistor;
+  } else if (currentMode === "VARISTOR") {
+    voltage = knob1Voltage[rotationsKnob1].varistor;
+  }
+  console.log("Voltage:", voltage); // sprawdzanko
+  return voltage;
+}
+
+function updateMultimetersDisplay() {
+  if (multimeter1Active) {
+  multimeterDisplay1.textContent = getDisplayValue(currentMultimeterMode);
+  }
+  if (multimeter2Active) {
+  multimeterDisplay2.textContent = getDisplayValue(currentMultimeterMode2);
+  }
+}
 
 knob1.addEventListener("mousedown", () => {
-  knob1.style.transform = `rotate(${rotationsKnob1 * 45}deg)`;
-  if (rotationsKnob1 === 8) rotationsKnob1 = 1;
+  if (rotationsKnob1 === 8) rotationsKnob1 = 0;
   else rotationsKnob1++;
+  knob1.style.transform = `rotate(${270 + rotationsKnob1 * 22.5}deg)`;
+  console.log("Rotations knob1:", rotationsKnob1); // sprawdzanko
+  updateMultimetersDisplay();
 });
 
 knob2.addEventListener("mousedown", () => {
@@ -69,8 +151,8 @@ knob2.addEventListener("mousedown", () => {
     currentMode = mode[rotationsKnob2];
     rotationsKnob2++;
   }
-
   console.log("Current Mode:", currentMode); // sprawdzanko
+  updateMultimetersDisplay();
   updateChartByMode();
 });
 
@@ -83,6 +165,8 @@ multimeterKnob1.addEventListener("mousedown", () => {
     currentMultimeterMode = multimeterMode[rotationsMultimeterKnob1].mode;
     rotationsMultimeterKnob1++;
   }
+  updateMultimetersDisplay();
+  console.log("Current Multimeter1 Mode:", currentMultimeterMode);
 });
 multimeterKnob2.addEventListener("mousedown", () => {
   multimeterKnob2.style.transform = `rotate(${multimeterMode[rotationsMultimeterKnob2].degree}deg)`;
@@ -93,6 +177,8 @@ multimeterKnob2.addEventListener("mousedown", () => {
     currentMultimeterMode2 = multimeterMode[rotationsMultimeterKnob2].mode;
     rotationsMultimeterKnob2++;
   }
+  updateMultimetersDisplay();
+  console.log("Current Multimeter2 Mode:", currentMultimeterMode);
 });
 const ctx = document.getElementById("chart").getContext("2d");
 chartInstance = new Chart(ctx, {
@@ -184,15 +270,16 @@ function updateLedState() {
         checkbox.disabled = true;
       });
 
-    rotationsKnob1 = 1;
+    rotationsKnob1 = 0;
     rotationsKnob2 = 1;
     rotationsMultimeterKnob1 = 1;
     rotationsMultimeterKnob2 = 1;
 
-    knob1.style.transform = "rotate(0deg)";
+    knob1.style.transform = "rotate(270deg)";
     knob2.style.transform = "rotate(0deg)";
     multimeterKnob1.style.transform = "rotate(0deg)";
     multimeterKnob2.style.transform = "rotate(0deg)";
+    updateMultimetersDisplay();
   }
 
   knob1.style.pointerEvents = isNetworkOn ? "auto" : "none";
