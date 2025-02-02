62f44c01c8a8d5d5c902311d23e7c2568eb3506e
Michu Najlepszy <michalnieradko10@gmail.com>
2025-02-02 20:18:06 +0100

    Added cable connection system with buttons and implemented multimeter mode selection based on connected inputs

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -4,6 +4,19 @@ const knob1 = document.querySelector(".knob1");
 const knob2 = document.querySelector(".knob2");
 const multimeterKnob1 = document.querySelector(".multimeterKnob1");
 const multimeterKnob2 = document.querySelector(".multimeterKnob2");
+
+//przyciski
+const btnBoxA = document.getElementById("btnBoxA");
+const btnBoxCOM1 = document.getElementById("btnBoxCOM1");
+const btnBoxV = document.getElementById("btnBoxV");
+const btnBoxCOM2 = document.getElementById("btnBoxCOM2");
+const btnMultimeter1A = document.getElementById("btnMultimeter1A");
+const btnMultimeter1COM = document.getElementById("btnMultimeter1COM");
+const btnMultimeter1V = document.getElementById("btnMultimeter1V");
+const btnMultimeter2A = document.getElementById("btnMultimeter2A");
+const btnMultimeter2COM = document.getElementById("btnMultimeter2COM");
+const btnMultimeter2V = document.getElementById("btnMultimeter2V");
+
 const mode = {
   0: "OFF",
   1: "PHOTORESISTOR",
@@ -49,12 +62,12 @@ const multimeterMode = {
 const knob1Voltage = {
   0: { varistor: 0, photoresistor: 0 },
   1: { varistor: 37.5, photoresistor: 1.5 },
-  2: { varistor: 37.5*2, photoresistor: 3 },
-  3: { varistor: 37.5*3, photoresistor: 5 },
-  4: { varistor: 37.5*4, photoresistor: 8 },
-  5: { varistor: 37.5*5, photoresistor: 11 },
-  6: { varistor: 37.5*6, photoresistor: 16 },
-  7: { varistor: 37.5*7, photoresistor: 20 },
+  2: { varistor: 37.5 * 2, photoresistor: 3 },
+  3: { varistor: 37.5 * 3, photoresistor: 5 },
+  4: { varistor: 37.5 * 4, photoresistor: 8 },
+  5: { varistor: 37.5 * 5, photoresistor: 11 },
+  6: { varistor: 37.5 * 6, photoresistor: 16 },
+  7: { varistor: 37.5 * 7, photoresistor: 20 },
   8: { varistor: 300, photoresistor: 24 },
 };
 
@@ -68,10 +81,9 @@ let currentMultimeterMode2 = multimeterMode[0].mode;
 let chartInstance;
 let multimeterDisplay1 = document.getElementById("multimeter1-value");
 let multimeterDisplay2 = document.getElementById("multimeter2-value");
-let multimeter1Active = true;
+let multimeter1Active = false;
 let multimeter2Active = true;
 let resistance = 1000;
-
 
 function getDisplayValue(currentMultimeterMode) {
   if (currentMode === "OFF") return "OFF";
@@ -89,29 +101,29 @@ function getDisplayValue(currentMultimeterMode) {
     case "DCV 1000":
       return calculateDisplayValue(getVoltage(), 1000, 0);
     case "DCA 20":
-      return calculateDisplayValue(getVoltage()/resistance, 20, 1)
+      return calculateDisplayValue(getVoltage() / resistance, 20, 1);
     case "DCA 2":
-      return calculateDisplayValue(getVoltage()/resistance, 2, 2)
+      return calculateDisplayValue(getVoltage() / resistance, 2, 2);
     case "DCA 200m":
-      return calculateDisplayValue(getVoltage()/resistance, 0.2, 3)
+      return calculateDisplayValue(getVoltage() / resistance, 0.2, 3);
     case "DCA 20m":
-      return calculateDisplayValue(getVoltage()/resistance, 0.02, 3)
+      return calculateDisplayValue(getVoltage() / resistance, 0.02, 3);
     case "DCA 2m":
-      return calculateDisplayValue(getVoltage()/resistance, 0.002, 3)
+      return calculateDisplayValue(getVoltage() / resistance, 0.002, 3);
     case "DCA 200u":
-      return calculateDisplayValue(getVoltage()/resistance, 0.0002, 3)
+      return calculateDisplayValue(getVoltage() / resistance, 0.0002, 3);
     default:
-      return 'OFF'
+      return "OFF";
   }
 }
 
-function calculateDisplayValue(value, range, fix){
+function calculateDisplayValue(value, range, fix) {
   if (value > range) return "0L";
   return valueToFixed(value, fix);
 }
 
 function valueToFixed(value, fix) {
-  if (typeof value === 'number') return value.toFixed(fix);
+  if (typeof value === "number") return value.toFixed(fix);
 }
 
 function getVoltage() {
@@ -127,10 +139,10 @@ function getVoltage() {
 
 function updateMultimetersDisplay() {
   if (multimeter1Active) {
-  multimeterDisplay1.textContent = getDisplayValue(currentMultimeterMode);
+    multimeterDisplay1.textContent = getDisplayValue(currentMultimeterMode);
   }
   if (multimeter2Active) {
-  multimeterDisplay2.textContent = getDisplayValue(currentMultimeterMode2);
+    multimeterDisplay2.textContent = getDisplayValue(currentMultimeterMode2);
   }
 }
 
@@ -168,6 +180,7 @@ multimeterKnob1.addEventListener("mousedown", () => {
   updateMultimetersDisplay();
   console.log("Current Multimeter1 Mode:", currentMultimeterMode);
 });
+
 multimeterKnob2.addEventListener("mousedown", () => {
   multimeterKnob2.style.transform = `rotate(${multimeterMode[rotationsMultimeterKnob2].degree}deg)`;
   if (rotationsMultimeterKnob2 === 11) {
@@ -178,8 +191,9 @@ multimeterKnob2.addEventListener("mousedown", () => {
     rotationsMultimeterKnob2++;
   }
   updateMultimetersDisplay();
-  console.log("Current Multimeter2 Mode:", currentMultimeterMode);
+  console.log("Current Multimeter2 Mode:", currentMultimeterMode2);
 });
+
 const ctx = document.getElementById("chart").getContext("2d");
 chartInstance = new Chart(ctx, {
   type: "line",
@@ -202,9 +216,9 @@ chartInstance = new Chart(ctx, {
         display: true,
         text: chartTitles.OFF,
         font: {
-          size: 25, // Zwiększenie rozmiaru czcionki tytułu
-          weight: "bold", // Grubość czcionki
-          family: "Arial", // Czcionka (opcjonalnie)
+          size: 25,
+          weight: "bold",
+          family: "Arial",
         },
       },
       legend: {
@@ -231,7 +245,7 @@ chartInstance = new Chart(ctx, {
 function updateChartByMode() {
   const title = chartTitles[currentMode] || "Charakterystyka niedostępna";
   const data = chartData[currentMode] || chartData.OFF;
-  console.log("Setting chart title:", title); // sprawdzanko
+  console.log("Setting chart title:", title);
   if (chartInstance) {
     chartInstance.data.labels = data.labels;
     chartInstance.data.datasets[0].data = data.data;
@@ -240,7 +254,7 @@ function updateChartByMode() {
   chartInstance.update();
 }
 
-//diuoda
+// dioda sieci
 const networkSwitch = document.getElementById("networkSwitch");
 const networkLed = document.getElementById("networkLed");
 
@@ -292,3 +306,212 @@ function updateLedState() {
 
 networkSwitch.addEventListener("change", updateLedState);
 updateLedState();
+
+// kabelki
+const CABLE_SEGMENTS = 5; // bugged
+let cable = null; // aktualnie tworzony kabel
+let startButton = null;
+let connections = [];
+
+// warstwa svg
+const svg = d3
+  .select("body")
+  .append("svg")
+  .attr("class", "cable-layer")
+  .attr("width", window.innerWidth)
+  .attr("height", document.documentElement.scrollHeight);
+
+// rysowanie prostej
+const simulationNodeDrawer = d3
+  .line()
+  .x((d) => d.x)
+  .y((d) => d.y)
+  .curve(d3.curveLinear);
+
+// dane dla przyciskow
+d3.selectAll(".roundButton").each(function () {
+  const btn = d3.select(this);
+  btn.datum({
+    type: btn.attr("data-type"),
+    connected: false,
+    connectionCable: null,
+  });
+});
+
+// bsługa zdarzen
+d3.selectAll(".roundButton")
+  .on("mousedown", function (event, d) {
+    event.stopPropagation();
+    if (d.connected) return;
+
+    startButton = this;
+    const rect = this.getBoundingClientRect();
+    const startX = rect.left + rect.width / 2 + window.scrollX;
+    const startY = rect.top + rect.height / 2 + window.scrollY;
+
+    const cableColor = d.type === "black" ? "black" : "#c43617";
+    let c = svg
+      .append("path")
+      .attr("stroke", cableColor)
+      .attr("fill", "none")
+      .attr("stroke-width", 10);
+
+    cable = c;
+
+    const nodes = d3.range(CABLE_SEGMENTS).map(() => ({}));
+    const links = d3
+      .pairs(nodes)
+      .map(([source, target]) => ({ source, target }));
+
+    nodes[0].fx = startX;
+    nodes[0].fy = startY;
+    for (let i = 1; i < nodes.length; i++) {
+      nodes[i].fx = startX;
+      nodes[i].fy = startY;
+    }
+
+    const sim = d3
+      .forceSimulation(nodes)
+      .force("links", d3.forceLink(links).strength(0.5))
+      .force("collide", d3.forceCollide(20))
+      .on("tick", () => {
+        c.attr("d", simulationNodeDrawer(nodes));
+      });
+
+    cable.datum({ nodes, sim, endpoints: [] });
+  })
+  .on("click", function (event, d) {
+    if (d.connected && d.connectionCable) {
+      removeConnection(this);
+      console.log(connections);
+    }
+  });
+
+d3.select(document).on("mousemove", (event) => {
+  if (cable) {
+    const { nodes, sim } = cable.datum();
+    const end = nodes[nodes.length - 1];
+
+    end.fx = event.clientX + window.scrollX;
+    end.fy = event.clientY + window.scrollY;
+
+    const start = nodes[0];
+    const distance = Math.sqrt(
+      (end.fx - start.fx) ** 2 + (end.fy - start.fy) ** 2
+    );
+    sim.force("links").distance(distance / CABLE_SEGMENTS);
+    sim.alpha(1).restart();
+  }
+});
+
+d3.select(document).on("mouseup", (event) => {
+  if (cable) {
+    if (event.target.tagName === "BUTTON" && event.target !== startButton) {
+      const targetData = d3.select(event.target).datum();
+      const startData = d3.select(startButton).datum();
+
+      if (targetData.connected || startData.type !== targetData.type) {
+        cable.remove();
+      } else {
+        const rect = event.target.getBoundingClientRect();
+        const endX = rect.left + rect.width / 2;
+        const endY = rect.top + rect.height / 2;
+
+        const { nodes, sim } = cable.datum();
+        const end = nodes[nodes.length - 1];
+        end.fx = endX;
+        end.fy = endY;
+        sim.alpha(1).restart();
+        sim.stop();
+
+        cable.datum().endpoints = [startButton, event.target];
+
+        startData.connected = true;
+        targetData.connected = true;
+        startData.connectionCable = cable;
+        targetData.connectionCable = cable;
+        d3.select(startButton).classed("connected", true);
+        d3.select(event.target).classed("connected", true);
+
+        connections.push({ start: startButton, end: event.target, cable });
+        let check1 = checkMultimeter1Status();
+        let check2 = checkMultimeter2Status();
+        console.log("Multimeter1 mode: " + check1);
+        console.log("Multimeter2 mode: " + check2);
+        console.log(connections);
+      }
+    } else {
+      cable.remove();
+    }
+    cable = null;
+    startButton = null;
+  }
+});
+
+// sprawdza tryb prawego multimetra
+function checkMultimeter2Status() {
+  const isConnectedToCOM = isConnected(btnMultimeter2COM, btnBoxCOM1);
+  const isConnectedToCOM2 = isConnected(btnMultimeter2COM, btnBoxCOM2);
+
+  if (!isConnectedToCOM && !isConnectedToCOM2) return "OFF";
+
+  const isConnectedToA = isConnected(btnMultimeter2A, btnBoxA);
+  const isConnectedToV = isConnected(btnMultimeter2V, btnBoxV);
+
+  if (isConnectedToA && isConnectedToV) {
+    return "ERROR";
+  } else if (isConnectedToA) {
+    return "A";
+  } else if (isConnectedToV) {
+    return "V";
+  } else {
+    return "OFF";
+  }
+}
+
+// prawdza tryb lewego multimetra
+function checkMultimeter1Status() {
+  const isConnectedToCOM = isConnected(btnMultimeter1COM, btnBoxCOM1);
+  const isConnectedToCOM2 = isConnected(btnMultimeter1COM, btnBoxCOM2);
+
+  if (!isConnectedToCOM && !isConnectedToCOM2) return "OFF";
+
+  const isConnectedToA = isConnected(btnMultimeter1A, btnBoxA);
+  const isConnectedToV = isConnected(btnMultimeter1V, btnBoxV);
+
+  if (isConnectedToA && isConnectedToV) {
+    return "ERROR";
+  } else if (isConnectedToA) {
+    return "A";
+  } else if (isConnectedToV) {
+    return "V";
+  } else {
+    return "OFF";
+  }
+}
+// sprawdzanie czy przyciski sa polaczone
+function isConnected(buttonA, buttonB) {
+  return connections.some(
+    (conn) =>
+      (conn.start === buttonA && conn.end === buttonB) ||
+      (conn.start === buttonB && conn.end === buttonA)
+  );
+}
+
+// usuwa polaczenie z przycisku
+function removeConnection(button) {
+  const toRemove = connections.filter(
+    (conn) => conn.start === button || conn.end === button
+  );
+
+  toRemove.forEach((conn) => {
+    conn.cable.remove();
+    d3.select(conn.start).datum().connected = false;
+    d3.select(conn.end).datum().connected = false;
+    d3.select(conn.start).datum().connectionCable = null;
+    d3.select(conn.end).datum().connectionCable = null;
+    d3.select(conn.start).classed("connected", false);
+    d3.select(conn.end).classed("connected", false);
+  });
+  connections = connections.filter((conn) => !toRemove.includes(conn));
+}
